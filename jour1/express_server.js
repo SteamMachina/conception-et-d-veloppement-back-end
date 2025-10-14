9fcb4b9491b34b85bab426ffbb451ad667b2944d
SteamMachina <52200491+SteamMachina@users.noreply.github.com>
2025-10-14 12:01:07 +0200

    6. Renvoyer du html

--- jour1/express_server.js
+++ jour1/express_server.js
@@ -3,7 +3,12 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  //res.send('Hello World!')
+  res.send('Hello World!')
+})
+
+app.get('/some-html', (req, res) => {
+    let code = `<html><body><h1>bonjour html</h1></body></html>`;
+  res.send(code)
 })
 
 app.listen(port, () => {
