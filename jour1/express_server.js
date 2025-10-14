d04e01679dc407c9dab71cf85a8f0d0233ab349e
SteamMachina <52200491+SteamMachina@users.noreply.github.com>
2025-10-14 12:09:57 +0200

    7. Renvoyer du JSON

--- jour1/express_server.js
+++ jour1/express_server.js
@@ -3,14 +3,22 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.get('/some-html', (req, res) => {
     let code = `<html><body><h1>bonjour html</h1></body></html>`;
-  res.send(code)
-})
+  res.send(code);
+});
+
+app.get('/some-json', (req, res) => {
+    const person = {
+        "age": 22, 
+        "nom" : "Jane"
+    };
+  res.json(person);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
