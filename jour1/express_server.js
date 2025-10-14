1498939e3d3934c0dc7aa60592a6afb0aed38c6f
SteamMachina <52200491+SteamMachina@users.noreply.github.com>
2025-10-14 14:15:23 +0200

    14. Url avec paramètre

--- jour1/express_server.js
+++ jour1/express_server.js
@@ -2,17 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// 4. Installation express et première utilisation
+// 10. L'objet request - exercice
 app.get('/', (req, res) => {
     console.log(req.headers);
     console.log(req.body);
   res.send('Hello World!');
 });
 
+// 6. Renvoyer du html
 app.get('/some-html', (req, res) => {
     let code = `<html><body><h1>bonjour html</h1></body></html>`;
   res.send(code);
 });
 
+// 7. Renvoyer du JSON
 app.get('/some-json', (req, res) => {
     const person = {
         "age": 22, 
@@ -21,15 +25,25 @@ app.get('/some-json', (req, res) => {
   res.json(person);
 });
 
+// 8. Renvoyer du JSON 2
 app.get('/transaction', (req, res) => {
     const numbers = [100, 2000, 3000];
   res.json(numbers);
 });
 
+// 13. Query string
 app.get('/exo-query-string', (req, res) => {
   const person = req.query;
   console.log(person);
   const message = `<h1>${person.age}</h1>`;
+  res.send(message);
+});
+
+// 14. Url avec paramètre
+app.get('/get-user/:userId', (req, res) => {
+  const user = req.params.userId;
+  console.log(user);
+  const message = `<h1>${user}</h1>`;
   res.send(message);
 });
 
